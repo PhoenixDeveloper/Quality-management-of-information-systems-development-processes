6ee1c86b47b337ff01b385357e01c50d6f6c6648
PhoenixKnight <mefisto40000x@gmail.com>
2020-01-21 21:02:44 +0300

    Создан тест на создание пользователя

---
+++ KRTestWork/KRTestWork/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KRTestWork
+{
+    class Tests
+    {
+        public void TestCreateUser()
+        {
+            User user = new User("login", "password");
+        }
+    }
+}
